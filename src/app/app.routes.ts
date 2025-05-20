167559131f0a6d8abe9815caf36220652e25b581
MauricioBarriosB <afasia.band@gmail.com>
2025-05-20 13:21:24 -0400

    acntualizacion tabla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 
+import { MaterialtableComponent } from './components/materialtable/materialtable.component';
+
 export const routes: Routes = [
     {
         path: '',
-        component: MainPageComponent
+       component: MainPageComponent
+        // component: MaterialtableComponent
     },
     {
         path: '**',
